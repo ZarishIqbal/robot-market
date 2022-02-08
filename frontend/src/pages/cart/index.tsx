0d0f21e70b6e184c793d440f3c81af6c043635c3
Zarish Iqbal <65783768+ZarishIqbal@users.noreply.github.com>
2022-02-08 22:17:57 +0300

    Chore: Clean up (#8)
    * add key to renderItem
    
    * add close cart button
    
    * edit readme
    
    * create error page
    
    * add routing

--- frontend/src/pages/cart/index.tsx
+++ frontend/src/pages/cart/index.tsx
@@ -6,8 +6,9 @@ interface CartProps {
 	robots: Robots
 	addToCart: (id: string) => void
 	removeFromCart: (id: string) => void
+	closeCart: () => void
 }
-const Cart = ({ robots, addToCart, removeFromCart }: CartProps) => {
+const Cart = ({ robots, addToCart, removeFromCart, closeCart }: CartProps) => {
 	const getTotalPrice = () => {
 		const totalPrice = Object.values(robots ?? {}).reduce(
 			(acc, curr) => acc + curr.price * curr.quantity,
@@ -23,8 +24,8 @@ const Cart = ({ robots, addToCart, removeFromCart }: CartProps) => {
 		return totalQuantity
 	}
 	return (
-		<div className="fixed pb-20 top-0 right-0 h-full w-96 z-40 bg-gray-100 p-2 pt-10 overflow-y-scroll space-y-5 border border-gray-200 shadow-xl">
-			{Object.values(robots ?? {}).map((robot, index) => (
+		<div className="fixed pb-20 top-0 right-0 h-full w-full md:w-96 z-40 bg-gray-100 p-2 pt-10 overflow-y-scroll space-y-5 border border-gray-200 shadow-xl">
+			{Object.values(robots ?? {}).map(robot => (
 				<CartItem
 					key={robot.id}
 					robot={robot}
@@ -32,7 +33,7 @@ const Cart = ({ robots, addToCart, removeFromCart }: CartProps) => {
 					removeFromCart={removeFromCart}
 				/>
 			))}
-			{getTotalPrice() > 0 && (
+			{getTotalPrice() > 0 ? (
 				<div>
 					<h2 className="text-black font-semibold">Order Summary</h2>
 					<p>Items: {getTotalQuantity()}</p>
@@ -41,7 +42,16 @@ const Cart = ({ robots, addToCart, removeFromCart }: CartProps) => {
 						<span className="text-teal-brand">{formatPrice(getTotalPrice())}</span>
 					</div>
 				</div>
+			) : (
+				<div className="flex w-full justify-center text-teal-brand font-bold">
+					Nothing in the cart!
+				</div>
 			)}
+			<button
+				onClick={() => closeCart()}
+				className="text-teal-brand justify-self-end bg-gray-200 w-10/12 items-center font-semibold rounded-md mx-5  h-12 mt-2">
+				Close
+			</button>
 		</div>
 	)
 }
