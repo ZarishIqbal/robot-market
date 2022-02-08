27eff3cc347fd1712fffcb0572121979279220df
Zarish Iqbal <zarishiqbal.zi@gmail.com>
2022-02-08 15:58:33 +0300

    add addToCart, removeFromCart and react toast for alerts

--- frontend/src/pages/cart/index.tsx
+++ frontend/src/pages/cart/index.tsx
@@ -1,9 +1,37 @@
+import CartItem from 'components/cart-item'
 import React from 'react'
+import { formatPrice } from 'utils/helpers'
 
-const Cart = () => {
+interface CartProps {
+	robots: Robots
+	addToCart: (id: string) => void
+	removeFromCart: (id: string) => void
+}
+const Cart = ({ robots, addToCart, removeFromCart }: CartProps) => {
+	const getTotal = () => {
+		const totalPrice = Object.values(robots ?? {}).reduce(
+			(acc, curr) => acc + curr.price * curr.quantity,
+			0
+		)
+		return totalPrice
+	}
 	return (
-		<div className="fixed top-0 right-0 h-full w-1/4 z-40 bg-white border border-gray-100 shadow-lg">
-			Your shopping Cart
+		<div className="fixed pb-20 top-0 right-0 h-full w-1/3 z-40 bg-gray-50 p-2 pt-10 overflow-y-scroll space-y-5 border border-gray-100 shadow-lg">
+			<h3 className="font-bold text-2xl">Your shopping cart</h3>
+			{Object.values(robots ?? {}).map((robot, index) => (
+				<div key={robot.id} className="flex items-center">
+					<p className="flex justify-center items-center w-8 h-8 m-2 border border-gray-600 shadow-lg rounded-md">
+						{index + 1}
+					</p>
+
+					<CartItem robot={robot} addToCart={addToCart} removeFromCart={removeFromCart} />
+				</div>
+			))}
+			{getTotal() > 0 && (
+				<p className="font-semibold text-teal-brand text-lg">
+					Grand Total: {formatPrice(getTotal())}
+				</p>
+			)}
 		</div>
 	)
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,21 +4,70 @@ import useAPIData from 'hooks/useApiData'
 import ProductList from './product'
 import useComponentVisible from 'hooks/useComponentVisible'
 import Cart from './cart'
+import toast from 'react-hot-toast'
 
 const App = () => {
 	const { robots } = useAPIData()
 
 	const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false)
-	const [cartItems, setCartItems] = React.useState<Robots>()
-	const addToCart = (id: string) => {}
-	const removeFromCart = (id: string) => {}
+	const [cartItems, setCartItems] = React.useState<Robots>({})
+
+	const addToCart = (id: string) => {
+		const selectedRobot = robots[id]
+		/*If robot is out of stock*/
+		if (selectedRobot.stock === 0) {
+			return toast.error('Out of stock!')
+		}
+		/*If robot already is in the cart */
+		if (cartItems?.[id]) {
+			/* Check is robot quantity in cart is not equal to stock*/
+			const inStock = cartItems[id].stock !== cartItems[id].quantity
+
+			/* If Robot in stock, increment quantity in cart*/
+			if (inStock) {
+				setCartItems(prev => ({
+					...prev,
+					[id]: { ...selectedRobot, quantity: prev[id].quantity + 1 }
+				}))
+				return toast.success(`${selectedRobot.name} added to cart`)
+			}
+
+			return toast.error('Maximum robot in stock added!')
+		}
+
+		/* If new robot type is added, check the number of types already in cart*/
+		if (Object.keys(cartItems).length < 5) {
+			setCartItems(prev => ({ ...prev, [id]: { ...selectedRobot, quantity: 1 } }))
+			return toast.success(`${selectedRobot.name} added to cart`)
+		}
+
+		return toast.error('Only 5 robots of different type can be purched in an order!')
+	}
+
+	const removeFromCart = (id: string) => {
+		const selectedRobot = robots[id]
+		setCartItems(prev => {
+			/* If more robot quantity >1 
+			then decrement quantity
+			else delete object from cart
+			*/
+			if (cartItems?.[id]?.quantity > 1) {
+				return { ...prev, [id]: { ...selectedRobot, quantity: prev[id].quantity - 1 } }
+			} else {
+				delete prev?.[id]
+				return { ...prev }
+			}
+		})
+	}
 
 	return (
 		<DefaultLayout toggleCart={() => setIsComponentVisible(!isComponentVisible)}>
 			<div ref={ref} className="w-full h-full z-50">
 				<ProductList robots={Object.values(robots ?? {})} addToCart={addToCart} />
 			</div>
-			{isComponentVisible && <Cart />}
+			{isComponentVisible && (
+				<Cart addToCart={addToCart} robots={cartItems} removeFromCart={removeFromCart} />
+			)}
 		</DefaultLayout>
 	)
 }
