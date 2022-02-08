47d65d34d0e823e1d0123b0f97fcb7101e1e083b
Zarish Iqbal <zarishiqbal.zi@gmail.com>
2022-02-08 15:27:38 +0300

    create cart component

---
+++ frontend/src/pages/cart/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Cart = () => {
+	return (
+		<div className="fixed top-0 right-0 h-full w-1/4 z-50 bg-white border border-gray-100 shadow-lg">
+			Your shopping Cart
+		</div>
+	)
+}
+export default Cart
