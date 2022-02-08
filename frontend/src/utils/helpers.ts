27d04cd3ac3f1f0b6c4cd2ea09faf45299199b8f
Zarish Iqbal <zarishiqbal.zi@gmail.com>
2022-02-08 15:15:47 +0300

    update process data

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -3,6 +3,15 @@ import { v4 as uuid } from 'uuid'
 export const processData = (robots: any[]): Robots => {
 	return robots.reduce((accumaltor, current) => {
 		const id = uuid()
-		return { ...accumaltor, [id]: { ...current, id: id, quantity: 0 } }
+		return {
+			...accumaltor,
+			[id]: {
+				...current,
+				id: id,
+				quantity: 0,
+				price: parseFloat(current.price),
+				stock: parseInt(current.stock)
+			}
+		}
 	}, {} as Robots)
 }
