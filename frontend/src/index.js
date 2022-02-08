84a1284ceb14138e067d6472afd9ab42b5f72c7f
Zarish Iqbal <zarishiqbal.zi@gmail.com>
2022-02-08 03:10:32 +0300

    import tailwind in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./pages";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './pages'
+import './styles/main.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
