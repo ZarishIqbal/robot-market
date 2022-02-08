ae4ac9b2fa463a862ce8ec29a00e8a5517c795fb
Zarish Iqbal <zarishiqbal.zi@gmail.com>
2022-02-08 03:58:36 +0300

    refactoring

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid'
 
-export const processData = (robot: any[]): Robots => {
-	return robot?.reduce((accumaltor, current) => {
+export const processData = (robots: any[]): Robots => {
+	return robots.reduce((accumaltor, current) => {
 		const id = uuid()
 		return { ...accumaltor, [id]: { ...current, id: id, quantity: 0 } }
 	}, {} as Robots)
