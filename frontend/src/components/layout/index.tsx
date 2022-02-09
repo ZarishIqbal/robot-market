0d68129f0cc746b17317d743ce17d258a3f16d59
Zarish97 <65783768+Zarish97@users.noreply.github.com>
2022-02-09 16:39:29 +0300

    test app layout

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import useDocumentTitle from 'hooks/useDocumentTitle'
-import AppHeader from './app-header'
+import React from "react";
+import useDocumentTitle from "hooks/useDocumentTitle";
+import AppHeader from "./app-header";
 
 interface AppLayoutProps {
-	children: React.ReactNode
-	title?: string
-	toggleCart: () => void
+  children: React.ReactNode;
+  title?: string;
+  toggleCart: () => void;
 }
 const DefaultLayout = ({ title, toggleCart, children }: AppLayoutProps) => {
-	const docTitle = title ? `${title} | Robo Market` : 'Robo Market'
-	useDocumentTitle(docTitle)
-	return (
-		<div className="w-full text-gray-700">
-			<AppHeader toggleCart={toggleCart} title={docTitle} />
-			{children}
-		</div>
-	)
-}
-export default DefaultLayout
+  const docTitle = title ? `${title} | Robo Market` : "Robo Market";
+  useDocumentTitle(docTitle);
+  return (
+    <div data-testid="app-layout" className="w-full text-gray-700">
+      <AppHeader toggleCart={toggleCart} title={docTitle} />
+      {children}
+    </div>
+  );
+};
+export default DefaultLayout;
